1d69a69fb38da75453f0ef9db5e8e7dcfc3167c1
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2026-04-13 17:16:20 +0200

    feat: update background color to #DBD2C3 across pages
    Former-commit-id: ef5192cbcc2380747f17484504418bea259ea750

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -63,7 +63,7 @@ const sections = [
 
 export default function MenuPage() {
   return (
-    <main className="min-h-screen pb-20" style={{ backgroundColor: "#FFEACD" }}>
+    <main className="min-h-screen pb-20" style={{ backgroundColor: "#DBD2C3" }}>
       {/* Header */}
       <div
         className="flex flex-col items-center gap-4 px-8 pb-16"
@@ -87,7 +87,7 @@ export default function MenuPage() {
           <span className="block w-16 h-px bg-[#6C270E]" />
         </div>
 
-        <Button href="/BH-2026-03-13_DLC_Menu_pasta_brunch.pdf" label="View Menu" />
+        <Button href="https://www.delacasapastabar.com/_files/ugd/430aec_4fe63c2d79264965a4e57cdf6a42fdcd.pdf" label="View Menu" external />
       </div>
 
       {/* Sections */}
