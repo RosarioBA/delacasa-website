a572448b2e89dd2460bb99417e65baa0809e295d
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2026-04-13 15:16:36 +0200

    feat: remove Bonita Cafe promo from home page
    Former-commit-id: a3e0f4fdbaa08def43ab4be1e64d0ed9f3adfc97

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/Hero";
 import Welcome from "@/components/Welcome";
-import BonitaPromo from "@/components/BonitaPromo";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Hero />
       <Welcome />
-      <BonitaPromo />
     </main>
   );
 }
